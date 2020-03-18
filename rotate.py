3854739142ca97c904c60098b4972fc543addf1c
Omkar Babrekar <47453834+Heisenberg3562@users.noreply.github.com>
2020-03-19 00:24:57 +0530

    Update rotate.py

--- rotate.py
+++ rotate.py
@@ -12,7 +12,7 @@ colorImage  = Image.open(input("Enter The image name:"))
 
 # Rotate it by 45 degrees
 
-#rotated     = colorImage.rotate(45)
+#rotated     = colorImage.rotate(180)
 
 # Rotate it by 90 degrees
 
